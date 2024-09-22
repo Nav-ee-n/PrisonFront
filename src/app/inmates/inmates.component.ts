cbc316ab493b74ef84de8bf1abbe723164695a5c
Nav-ee-n <naveenfelix876@gmail.com>
2024-09-22 22:42:03 +0530

    Initial commit

---
+++ src/app/inmates/inmates.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inmates',
+  standalone: true,
+  imports: [],
+  templateUrl: './inmates.component.html',
+  styleUrl: './inmates.component.css'
+})
+export class InmatesComponent {
+
+}
---
+++ src/app/visitors/visitors.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-visitors',
+  standalone: true,
+  imports: [],
+  templateUrl: './visitors.component.html',
+  styleUrl: './visitors.component.css'
+})
+export class VisitorsComponent {
+
+}
